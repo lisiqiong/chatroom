010fc710079cd0ab9a035cd5d960559d91d2566e
巴八灵 <peterlee20100427@yeah.net>
2018-08-06 10:52:26 +0800

    Update db.php

--- conf/db.php
+++ conf/db.php
@@ -5,7 +5,7 @@ return [
         "host"=>"192.168.0.213",
         "port"=>"3306",
         "user"=>"root",
-        "password"=>"devpassword",
+        "password"=>"123456",
         "database"=>"test",
         "charset"=>"utf8",
         "timeout"=>2,//可选链接超时时间
