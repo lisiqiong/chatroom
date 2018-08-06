2352d10f638bdc83fca47fafa8af8134d5b886a2
lisiqiong <peterlee20100427@yeah.net>
2018-08-06 11:15:54 +0800

    update .gitignore

---
+++ conf/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "mysql"=>[
+        "host"=>"192.168.0.213",
+        "port"=>"3306",
+        "user"=>"root",
+        "password"=>"123456",
+        "database"=>"test",
+        "charset"=>"utf8",
+        "timeout"=>2,//可选链接超时时间
+    ]
+];
